4913534eb38aac0cd53eff21f881336ef9721c76
Emmyy882 <emmaagbaje3@gmail.com>
2023-11-23 16:07:16 +0100

    update

--- models/state.py
+++ models/state.py
@@ -15,8 +15,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False) if getenv(
         "HBNB_TYPE_STORAGE") == 'db' else ''
-    cities = relationship("City",  backref="state", cascade="delete")
-    if getenv(
+    cities = relationship("City",  backref="state", cascade="delete") if getenv(
         "HBNB_TYPE_STORAGE") == 'db' else ''
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
