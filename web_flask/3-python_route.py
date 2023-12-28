4c2573d470188ccb8c54f26cc1deab439eef49e6
Emmyy882 <emmaagbaje3@gmail.com>
2023-12-28 20:56:46 +0100

    web flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A Script that starts a flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """Home Page"""
+    return f'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """HBNB Page"""
+    return f'HBNB'
+
+
+@app.route('/c/<text>')
+def text(text):
+    """text page - displays 'C' followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+@app.route('/python', defaults={'text': 'is cool'})
+def python_page(text):
+    """Python page: displays 'Python' followed by the value of the text"""
+    return 'python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
